bc33d9f7d8d8b741bf8c5bf2a949f5f1e010754d
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-02 00:28:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma (hi)
   if hi == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 
-elsif hi == hi.upcae
+if hi == hi.upcae
 return "NO, NOT SINCE 1938!"
 elsif hi != hi.upcase
 return "HUH?! SPEAK UP, SONNY!"
