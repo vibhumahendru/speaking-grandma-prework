c038bb9bd943ae2f775f3a04d5b02294cde0bb03
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-02 00:23:16 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,9 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma (hi)
+  if hi == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
 
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
